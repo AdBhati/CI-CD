b208f179fbf4ff82b4491fa2f98e1260358fb89c
Divyanshu Bhati <86517029+AdBhati@users.noreply.github.com>
2023-11-01 01:36:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
+app.get('/test', (req, res) => {
   res.json({
-    msg:'Hello World...This is update message from shakib khan'
+    msg:'Hello World...This is test end point'
     });
 });
 
 app.get('/test/show', (req, res) => {
   res.json({
-    msg:'Hello World...This is update message'
+    msg:'Hello World...This is show endpoint'
     });
 });
 
